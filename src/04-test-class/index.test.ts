6e9a5e947468689633d078b67e72c20b239b0d6c
alexb997 <alex_alexandru_997@yahoo.ro>
2025-05-08 20:52:47 +0300

    error handling & testing class

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,33 +1,55 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import { getBankAccount, InsufficientFundsError, TransferFailedError } from '.';
 
 describe('BankAccount', () => {
   test('should create account with initial balance', () => {
     // Write your test here
+    const account = getBankAccount(100);
+    expect(account.getBalance()).toBe(100);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
     // Write your test here
+    const account = getBankAccount(50);
+    expect(() => account.withdraw(100)).toThrow(InsufficientFundsError);
   });
 
   test('should throw error when transferring more than balance', () => {
     // Write your test here
+    const account1 = getBankAccount(50);
+    const account2 = getBankAccount(100);
+    expect(() => account1.transfer(80, account2)).toThrow(
+      InsufficientFundsError,
+    );
   });
 
   test('should throw error when transferring to the same account', () => {
     // Write your test here
+    const account1 = getBankAccount(50);
+    expect(() => account1.transfer(80, account1)).toThrow(TransferFailedError);
   });
 
   test('should deposit money', () => {
     // Write your test here
+    const account = getBankAccount(100);
+    account.deposit(50);
+    expect(account.getBalance()).toBe(150);
   });
 
   test('should withdraw money', () => {
     // Write your test here
+    const account = getBankAccount(100);
+    account.withdraw(50);
+    expect(account.getBalance()).toBe(50);
   });
 
   test('should transfer money', () => {
     // Write your test here
+    const account1 = getBankAccount(50);
+    const account2 = getBankAccount(100);
+    account1.transfer(30, account2);
+    expect(account2.getBalance()).toBe(130);
+    expect(account1.getBalance()).toBe(20);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
